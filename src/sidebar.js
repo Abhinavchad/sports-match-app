e6018768fffb4c4a0aa20d4d64910ecc6a32c967
Abhinav <abhinav.chaudhary.mec22@itbhu.ac.in>
2023-08-18 00:45:05 +0530

    Add sport stickers to sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
-const sports = ['Football', 'Basketball', 'Tennis','Badminton','Athletics','Boxing','Chess','Cricket','Cycling','Handball','Hockey','Kabaddi','Kho-Kho','Powerlifting','Squash','Table Tennis','Taekwondo','Volleyball','Weightlifiting'];
+const sports = [
+  { name: 'Football', sticker: 'football.png' },
+  { name: 'Basketball', sticker: 'basketball.png' },
+  { name: 'Tennis', sticker: 'tennis.png' },
+  { name: 'Badminton', sticker: 'player.png' },
+  { name: 'Athletics', sticker: 'runner.png' },
+  { name: 'Boxing', sticker: 'boxing-gloves.png' },
+  { name: 'Chess', sticker: 'chess.png' },
+  { name: 'Cricket', sticker: 'cricket.png' },
+  { name: 'Cycling', sticker: 'cycling.png' },
+  { name: 'Handball', sticker: 'handball.png' },
+  { name: 'Hockey', sticker: 'field-hockey.png' },
+  { name: 'Kabaddi', sticker: 'kabaddi.png' },
+  { name: 'Kho-Kho', sticker: 'game.png' },
+  { name: 'Squash', sticker: 'squash.png' },
+  { name: 'Table Tennis', sticker: 'table-tennis.png' },
+  { name: 'Taekwondo', sticker: 'taekwondo.png' },
+  { name: 'Volleyball', sticker: 'block.png' },
+  { name: 'Weightlifiting', sticker: 'weightlifter.png' },
+];
 
 const Sidebar = ({ selectedSport, setSelectedSport }) => {
   return (
     <div className="sidebar">
       {sports.map(sport => (
         <div
-          key={sport}
-          className={`sidebar-item ${selectedSport === sport ? 'selected' : ''}`}
-          onClick={() => setSelectedSport(sport)}
+          key={sport.name}
+          className={`sidebar-item ${selectedSport === sport.name ? 'selected' : ''}`}
+          onClick={() => setSelectedSport(sport.name)}
         >
-          {sport}
+          <img src={`/sport-stickers/${sport.sticker}`} alt={`${sport.name} Sticker`} />
+          {sport.name}
         </div>
       ))}
     </div>
